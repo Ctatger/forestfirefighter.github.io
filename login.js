cb59016c151e3499d03371372089be0abb155c0c
axelj23 <62571771+axelj23@users.noreply.github.com>
2022-01-04 10:51:21 +0100

    Add files via upload

--- login.js
+++ login.js
@@ -1,27 +1,14 @@
-
-var login = new Boolean(false);
-
 function validate()
 {
     var username=document.getElementById("username").value;
     var password=document.getElementById("password").value;
-    
-
-    if(username=="INSA" && password=="Zilly"){
-        alert("login ui");
-        login=true;
-        window.location.href = "gps.html";
-        return true;
-    }
-
-    else{
-        window.location.href = "login.html";
+    if(username=="INSA" && password=="Zilly")
+    {
+        alert("login succesfully");
         return false;
     }
-}
-
-function verify(){
-    if(login==false){
-        window.location.href = "login.html";
+    else
+    {
+        alert("login failed");
     }
 }
